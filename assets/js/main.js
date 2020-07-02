8d691c182497a908a189604518c341183288392b
sfdevelop <sfdevelop747@gmail.com>
2020-07-02 18:16:38 +0300

    02

--- assets/js/main.js
+++ assets/js/main.js
@@ -17753,54 +17753,87 @@ return Popper;
 
 }));
 
-		$(document).ready(function () {
-			//lazyload Ленивая загрузка делаем так чтобы повысить скорость загрузки сайта для гугл speed		
+//Крепим меню при прокрутке
+$(document).ready(function () {
+
+	var $menu = $("#menu");
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100 && $menu.hasClass("menu")) {
+			$menu.addClass("fixedsf");
+		} else if ($(this).scrollTop() <= 100 && $menu.hasClass("fixedsf")) {
+			$menu.removeClass("fixedsf").addClass("menu");
+		}
+	}); //scroll 
+});
+
+
+//боковое  меню
+$(document).ready(function () {
+	$("#navToggle").click(function () {
+		$(this).toggleClass("active");
+		$(".overlay").toggleClass("open");
+		// this line ▼ prevents content scroll-behind
+		$("body").toggleClass("locked");
+	});
+});
+
+$(document).ready(function () {
+	//lazyload Ленивая загрузка делаем так чтобы повысить скорость загрузки сайта для гугл speed		
 		[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
-				img.setAttribute('src', img.getAttribute('data-src'));
-				img.onload = function () {
-					img.removeAttribute('data-src');
-				};
-			});
-		});
+		img.setAttribute('src', img.getAttribute('data-src'));
+		img.onload = function () {
+			img.removeAttribute('data-src');
+		};
+	});
+});
 
-
-		$(document).ready(function () {
-			$('.slider').slick({
-				dots: true,
-				infinite: true,
-				speed: 300,
-				slidesToShow: 4,
-				slidesToScroll: 1,
-				responsive: [
-					{
-						breakpoint: 1024,
-						settings: {
-							slidesToShow: 3,
-							slidesToScroll: 3,
-							infinite: true,
-							dots: true
-						}
+//slider
+$(document).ready(function () {
+	$('.slider').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true
+				}
     },
-					{
-						breakpoint: 600,
-						settings: {
-							slidesToShow: 2,
-							slidesToScroll: 2
-						}
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1,
+					arrows: false,
+					slidesToScroll: 1
+				}
     },
-					{
-						breakpoint: 480,
-						settings: {
-							slidesToShow: 1,
-							slidesToScroll: 1
-						}
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					arrows: false,
+					slidesToScroll: 1
+				}
     }
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
     // instead of a settings object
   ]
-			});
-		});
+	});
+});
+
+
+$('.mobile_li').click(function (ev) {
+	$(this).find('>ul').slideToggle();
+	ev.stopPropagation();
+});
 /*!
   hey, [be]Lazy.js - v1.8.2 - 2016.10.25
   A fast, small and dependency free lazy load script (https://github.com/dinbror/blazy)
